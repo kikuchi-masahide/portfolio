b89c7a2f4d9e2b8474b1a55c73fc6f2a4d175f60
菊池雅英 <m20000406k@gmail.com>
2023-10-12 19:35:57 +0900

    vite.config.tsの設定を変更してリトライ

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    root: "src",
+    build: {
+        outDir: "..",
+        assetsDir: "./dist",
+        rollupOptions: {
+            input: {
+                main: "src/index.html",
+            },
+        },
+    },
 });
