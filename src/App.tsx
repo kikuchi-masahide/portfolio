42b63e9115c70a86c73336b966659bd204ba26f0
菊池雅英 <m20000406k@gmail.com>
2023-10-14 01:30:05 +0900

    画面上にスクロールするボタンを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import ReactMarkdown from "react-markdown";
 import markdown from "./assets/markdown.md?raw";
+import rehypeRaw from "rehype-raw";
 
 function App() {
+    const onclick = () => {
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
     return (
         <>
-            <p className="m-4 text-xl font-bold underline">Portfolio</p>
-            <ReactMarkdown className="markdown">{markdown}</ReactMarkdown>
+            <div className="m-4">
+                <ReactMarkdown className="markdown" rehypePlugins={[rehypeRaw]}>
+                    {markdown}
+                </ReactMarkdown>
+            </div>
+            <div
+                className="border border-gray-400 rounded-full bg-gray-200 opacity-50 p-2 w-12 h-12 fixed bottom-4 right-4 text-sm flex items-center justify-items-center"
+                onClick={onclick}
+            >
+                <p className="w-full h-full text-center p-1">top</p>
+            </div>
         </>
     );
 }
